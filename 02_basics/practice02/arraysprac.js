af6a5ce7c87372ea91787764b4068f376651a4a8
Mohammed Samiuddin <hsamiuddin405@gmail.com>
2024-03-24 00:15:23 +0530

    Update arraysprac.js
    BASIC ARRAYS

---
+++ 02_basics/practice02/arraysprac.js
@@ -0,0 +1,12 @@
+const myNewArr = [2,3,4,5,6]
+const heros = ["Kali","Panu"]
+
+const arr2 = new Array(2,3,4,5,6)
+console.log (myNewArr[4])
+
+
+/*myNewArr.pop()
+myNewArr.push(3);*/ // removes the last element
+myNewArr.unshift(10) // places the 2 in the front of the array 
+myNewArr.shift() // starting ka element nikalta
+console.log(myNewArr)
