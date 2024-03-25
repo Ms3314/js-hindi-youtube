43cb7f082c2fd3603064fb4ca33b32ff2a699a46
Mohammed Samiuddin <hsamiuddin405@gmail.com>
2024-03-25 22:31:55 +0530

    comments

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -10,11 +10,11 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(allHeros);
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
-
+// nice way to just join down the array
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
-
+// flat is an amazing methoods like if you have an array inside an array this helps alot to put all of them in one of an array 
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
@@ -29,3 +29,9 @@ let score2 = 200
 let score3 = 300
 
 console.log(Array.of(score1, score2, score3));
+// so we have three properties mainly 
+/* isArray (checks if it is an array)
+ *from (converts it into an array)
+ *of (So takes in variable input in return it in the form of an array  )
+ all of these work like array.yadayada()
+ */
